dcc7a53d4b35174190ea56435866bdd29613c825
devilpk50 <devilpk50@gmail.com>
2023-08-03 17:28:26 +0545

    post can be added, edited and status field added to post

--- post/models.py
+++ post/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.conf import settings
+
+
+class PostStatus(models.TextChoices):
+    PUBLIC = ("public", "Public")
+    PRIVATE = ("private", "Private")
 
 class Post(models.Model):
-    picture = models.ImageField(upload_to="post/")
+    picture = models.ImageField(upload_to="posts/")
     caption = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    status = models.CharField(max_length=100, choices=PostStatus.choices, default=PostStatus.PRIVATE)
 
     class Meta:
         verbose_name = "post"
@@ -14,7 +22,8 @@ class Post(models.Model):
 
 class Like(models.Model):
     is_Liked = models.BooleanField(default=False)
-    posts = models.ForeignKey(post, on_delete=models.CASCADE)
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return str(self.id)
@@ -23,6 +32,7 @@ class Like(models.Model):
 class Comment(models.Model):
     text = models.CharField(max_length=150)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.text
